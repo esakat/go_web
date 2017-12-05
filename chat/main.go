63703d8a6a873c65d9ff3f60ed4d01b55efa5979
tomoyaRed <esaka.tom@gmail.com>
2017-12-05 20:17:06 +0900

    gravatar version

--- chat/main.go
+++ chat/main.go
@@ -47,10 +47,21 @@ func main() {
 	gomniauth.WithProviders(
 		google.New(clientID, clientSecret, "http://localhost:8080/auth/callback/google"),
 	)
-	r := newRoom()
+	// UseAuthAvatar or UseGravatar
+	r := newRoom(UseGravatar)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
 	http.Handle("/room", r)
 	go r.run()
 
