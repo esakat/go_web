5c3e11925398a1aa5e14cec0a4817d17cee3d226
esakat <esaka.tom@gmail.com>
2018-03-17 21:41:42 +0900

    add test

---
+++ chap7/meander/cost_level_test.go
@@ -0,0 +1,17 @@
+package meander_test
+
+import (
+	"testing"
+
+	"../meander"
+	"github.com/cheekybits/is"
+)
+
+func TestCostValue(t *testing.T) {
+	is := is.New(t)
+	is.Equal(int(meander.Cost1), 1)
+	is.Equal(int(meander.Cost2), 2)
+	is.Equal(int(meander.Cost3), 3)
+	is.Equal(int(meander.Cost4), 4)
+	is.Equal(int(meander.Cost5), 5)
+}
