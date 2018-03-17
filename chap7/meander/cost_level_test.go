f3d66a6c732e9dd62915758e2b20077016a200ad
esakat <esaka.tom@gmail.com>
2018-03-17 22:05:10 +0900

    add String method

--- chap7/meander/cost_level_test.go
+++ chap7/meander/cost_level_test.go
@@ -18,9 +18,9 @@ func TestCostValue(t *testing.T) {
 
 func TestCostString(t *testing.T) {
 	is := is.New(t)
-	is.Equal(int(meander.Cost1.String()), "$")
-	is.Equal(int(meander.Cost2.String()), "$$")
-	is.Equal(int(meander.Cost3.String()), "$$$")
-	is.Equal(int(meander.Cost4.String()), "$$$$")
-	is.Equal(int(meander.Cost5.String()), "$$$$$")
+	is.Equal(meander.Cost1.String(), "$")
+	is.Equal(meander.Cost2.String(), "$$")
+	is.Equal(meander.Cost3.String(), "$$$")
+	is.Equal(meander.Cost4.String(), "$$$$")
+	is.Equal(meander.Cost5.String(), "$$$$$")
 }
