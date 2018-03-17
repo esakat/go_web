8ba3469987a4a85aa17e322475c9fc5f258c4af6
esakat <esaka.tom@gmail.com>
2018-03-17 22:08:29 +0900

    add ParseTest

--- chap7/meander/cost_level_test.go
+++ chap7/meander/cost_level_test.go
@@ -3,6 +3,8 @@ package meander_test
 import (
 	"testing"
 
+	"github.com/oreilly-japan/go-programming-blueprints/chapter7/meander"
+
 	"../meander"
 	"github.com/cheekybits/is"
 )
@@ -23,4 +25,13 @@ func TestCostString(t *testing.T) {
 	is.Equal(meander.Cost3.String(), "$$$")
 	is.Equal(meander.Cost4.String(), "$$$$")
 	is.Equal(meander.Cost5.String(), "$$$$$")
+}
+
+func TestParseCost(t *testing.T) {
+	is := is.New(t)
+	is.Equal(meander.Cost1, meander.ParseCost("$"))
+	is.Equal(meander.Cost2, meander.ParseCost("$$"))
+	is.Equal(meander.Cost3, meander.ParseCost("$$$"))
+	is.Equal(meander.Cost4, meander.ParseCost("$$$$"))
+	is.Equal(meander.Cost5, meander.ParseCost("$$$$$"))
 }
