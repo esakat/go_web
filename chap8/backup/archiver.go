bd421fd549d8c248e0a83bd4032084ddef93d971
esakat <esaka.tom@gmail.com>
2018-03-18 12:13:04 +0900

    add destfmt

--- chap8/backup/archiver.go
+++ chap8/backup/archiver.go
@@ -2,12 +2,14 @@ package backup
 
 import (
 	"archive/zip"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
 )
 
 type Archiver interface {
+	DestFmt() func(int64) string
 	Archive(src, dest string) error
 }
 
@@ -46,4 +48,10 @@ func (z *zipper) Archive(src, dest string) error {
 		io.Copy(f, in)
 		return nil
 	})
+}
+
+func (z *zipper) DestFmt() func(int64) string {
+	return func(i int64) string {
+		return fmt.Sprintf("%d.zip", i)
+	}
 }
